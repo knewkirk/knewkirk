94390e7414f880e7ca19718b996513d815ff14bc
Kyle Newkirk <kylednewkirk@gmail.com>
2023-05-08 16:51:13 -0700

    Add default cube
    - install deps
    -

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React, { Suspense } from 'react';
+import { OrbitControls, Stats, Loader } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { Leva, useControls } from 'leva';
+
+import './index.less';
+
+export default () => {
+  let devMode = false;
+  if (typeof window !== 'undefined') {
+    const params = new URLSearchParams(document.location.search);
+    devMode = !!params.get('dev');
+  }
+
+  const { color } = useControls({ color: { value: '#bbbbbb' } });
+
+  return (
+    <>
+      <Canvas dpr={[1, 2]}>
+        <OrbitControls />
+        <Suspense fallback={null}>
+          <mesh>
+            <boxGeometry />
+            <meshBasicMaterial color={color} />
+          </mesh>
+        </Suspense>
+      </Canvas>
+      <Leva hidden={PRODUCTION && !devMode} />
+      {(!PRODUCTION || devMode) && <Stats />}
+      <Loader />
+    </>
+  );
+};
